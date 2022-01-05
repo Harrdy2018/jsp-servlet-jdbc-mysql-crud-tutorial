5649b1f8017f11eb62eadb599d546688b7efc27b
lukang-local <lukang-local@example.com>
2022-01-05 23:59:18 +0800

    add get response head

--- src/main/java/net/javaguides/LearnHttpClient/Example2.java
+++ src/main/java/net/javaguides/LearnHttpClient/Example2.java
@@ -1,6 +1,5 @@
 package net.javaguides.LearnHttpClient;
 
-import org.apache.http.Header;
 import org.apache.http.HttpEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
@@ -9,15 +8,17 @@ import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
 
 import java.io.IOException;
+import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
 
-public class Example1 {
-    public static void main(String[] args){
+public class Example2 {
+    public static void main(String[] args) throws Exception{
         CloseableHttpClient client = HttpClients.createDefault();
-        String urlStr = "https://www.baidu.com/";
+        String param = "abc+123|wer";
+        // 要想后台拿到的参数和发送的一样，需要做urlencode，如果是浏览器的话，浏览器会自动帮我们做
+        param = URLEncoder.encode(param, StandardCharsets.UTF_8.name());
+        String urlStr = "http://localhost:8080/jsp_servlet_jdbc_mysql_crud_tutorial/test/query?name="+param;
         HttpGet httpget = new HttpGet(urlStr);
-        // 防盗链
-        httpget.addHeader("Referer", "https://www.baidu.com/");
         httpget.setHeader("User-Agent",
                 "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36");
 
@@ -25,14 +26,8 @@ public class Example1 {
         try {
             // CloseableHttpResponse 接口实现类 HttpResponseProxy
             response = client.execute(httpget);
-            // 常量响应码  HttpStatus
-            System.out.println(response.getStatusLine().getStatusCode());
-            for (Header header: response.getAllHeaders()) {
-                System.out.println(header);
-            }
             // HttpEntity  接口实现类 DecompressingEntity
             HttpEntity entity = response.getEntity();
-            System.out.println(entity.getContentType());
             System.out.println(EntityUtils.toString(entity, StandardCharsets.UTF_8));
             EntityUtils.consume(entity);
         } catch (Exception e) {
@@ -56,3 +51,4 @@ public class Example1 {
         }
     }
 }
+
